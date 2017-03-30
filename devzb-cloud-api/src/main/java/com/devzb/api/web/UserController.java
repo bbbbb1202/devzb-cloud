0ba6c462b25192c0a55a74b88e81b2e0365777e0
zhangbin <zhangbin@kakasure.com>
2017-03-30 12:29:30 +0800

    把框架单独移出来

--- devzb-cloud-api/src/main/java/com/devzb/api/web/UserController.java
+++ devzb-cloud-api/src/main/java/com/devzb/api/web/UserController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devzb.api.dao.model.SsoUser;
@@ -24,8 +23,9 @@ public class UserController extends BaseController {
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping(value = "", method = RequestMethod.GET)
+	@RequestMapping(value = "")
 	public List<SsoUser> readUserInfo() {
+		log.info("访问了/user...");
 		List<SsoUser> users = userService.getUsers();
 		return users;
 	}
